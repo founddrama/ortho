dd378f9b5bc32b4f2dc647e2e6ba87bb16b9c28d
Rob Friesel <rob@founddrama.net>
2009-05-16 08:06:17 -0400

    added call to footer()

--- footer.php
+++ footer.php
@@ -37,7 +37,8 @@
 <div id="license">
 	<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/us/80x15.png"/></a>&nbsp;This <span xmlns:dc="http://purl.org/dc/elements/1.1/" href="http://purl.org/dc/dcmitype/Text" rel="dc:type">work</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License</a>.
 </div>
-</div><?php /* #page */?>
+</div><?php /* #page */ ?>
 <?php include (TEMPLATEPATH . '/external-scripts.php') ?>
+<?php wp_footer(); ?>
 </body>
 </html>

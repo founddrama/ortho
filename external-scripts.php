32b2e9572799cc99f45e51b51262ff90e42737e3
Rob Friesel <rob@founddrama.net>
2010-05-13 21:02:00 -0400

    rewinding - plugin includes these automatically

--- external-scripts.php
+++ external-scripts.php
@@ -4,10 +4,3 @@
  ?>
 <script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
 <script text="text/javascript" src="http://twitter.com/statuses/user_timeline/founddrama.json?callback=twitterCallback2&count=5"></script>
-<link type="text/css" rel="stylesheet" href="/wp-content/plugins/google-syntax-highlighter/Styles/SyntaxHighlighter.css"></link>
-<script text="text/javascript" src="/wp-content/plugins/google-syntax-highlighter/Scripts/shCore.js"></script>
-<script text="text/javascript" src="/wp-content/plugins/google-syntax-highlighter/Scripts/shBrushJScript.js"></script>
-<script language="javascript">
-	dp.SyntaxHighlighter.ClipboardSwf = "/wp-content/plugins/google-syntax-highlighter/Scripts/clipboard.swf";
-	dp.SyntaxHighlighter.HighlightAll('code');
-</script>

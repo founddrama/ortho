3c835ef8292953ad63200e0dec6220f9d9f6d0c2
Rob Friesel <robf@dealer.com>
2010-07-27 12:32:03 -0400

    limit Tumblr api to 5, use pack of jcarousel

--- external-scripts.php
+++ external-scripts.php
@@ -4,10 +4,10 @@
 /* <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.js"></script> */
 ?>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jcarousellite.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jcarousellite.pack.js"></script>
 <script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
 <script type="text/javascript" src="http://twitter.com/statuses/user_timeline/founddrama.json?callback=twitterCallback2&count=5"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/fd-blog-scripts.min.js"></script>
-<script type="text/javascript" src="http://notanotherrob.tumblr.com/api/read/json?num=10&callback=F_D.notanotherrobOut"></script>
-<script type="text/javascript" src="http://notundefined.tumblr.com/api/read/json?num=10&callback=F_D.notundefinedOut"></script>
-<script type="text/javascript" src="http://notnonfiction.tumblr.com/api/read/json?num=10&callback=F_D.notnonfictionOut"></script>
+<script type="text/javascript" src="http://notanotherrob.tumblr.com/api/read/json?num=5&callback=F_D.notanotherrobOut"></script>
+<script type="text/javascript" src="http://notundefined.tumblr.com/api/read/json?num=5&callback=F_D.notundefinedOut"></script>
+<script type="text/javascript" src="http://notnonfiction.tumblr.com/api/read/json?num=5&callback=F_D.notnonfictionOut"></script>
